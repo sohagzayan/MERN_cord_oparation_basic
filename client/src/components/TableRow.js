4bd95a3a12a9b475a87d5ab27987a057dbb28ede
sohagmia360 <gdsohag360@gmail.com>
2022-04-22 11:58:19 +0600

    add new future on add from data on server

--- client/src/components/TableRow.js
+++ client/src/components/TableRow.js
@@ -1,32 +1,35 @@
-import React from 'react';
+import React from "react";
 
-const TableRow = ({item}) => {
-    const {name , email , gender , status ,_id} = item
+const TableRow = ({ item }) => {
+  const { name, email, gender, status, _id } = item;
 
-    const handleDeletedUser = async(id)=>{
-
-      try{
-        fetch("http://localhost:5000/api/user/"+ id ,{
-         method : 'DELETE'
-       })
-      }catch(err){
-        console.log(err.message);
-      }
-    
+  const handleDeletedUser = async (id) => {
+    try {
+      fetch("http://localhost:5000/api/user/" + id, {
+        method: "DELETE",
+      });
+    } catch (err) {
+      console.log(err.message);
     }
-    return (
-        <tr>
-        <td>1</td>
-        <td>{name}</td>
-        <td>{email}</td>
-        <td>{gender}</td>
-        <td>{status}</td>
-        <td>
-          <button onClick={()=> handleDeletedUser(_id)} className="btn btn-danger mx-3">DELETE</button>
-          <button className="btn btn-success mx-3">EDIT</button>
-        </td>
-      </tr>
-    );
+  };
+  return (
+    <tr>
+      <td>1</td>
+      <td>{name}</td>
+      <td>{email}</td>
+      <td>{gender}</td>
+      <td>{status}</td>
+      <td>
+        <button
+          onClick={() => handleDeletedUser(_id)}
+          className="btn btn-danger mx-3"
+        >
+          DELETE
+        </button>
+        <button className="btn btn-success mx-3">EDIT</button>
+      </td>
+    </tr>
+  );
 };
 
 export default TableRow;
--- client/src/pages/AddNewUser.js
+++ client/src/pages/AddNewUser.js
@@ -1,33 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 const AddNewUser = () => {
+
+  const [name , setName] = useState('')
+  const [email , setEmail] = useState('')
+  const [gender , setGender] = useState('gender')
+  const [status , setStatus] = useState('status')
+
+  const handleAddUser = async(e)=>{
+    e.preventDefault()
+
+    const userData = {
+        name ,
+        email,
+        gender,
+        status
+    }
+
+    await fetch('http://localhost:5000/api/user',{
+      method : "POST",
+      headers : {
+        'Content-Type': 'application/json',
+      },
+      body : JSON.stringify(userData)
+    })
+    .then(()=> console.log('adding successfull'))
+    .catch((err)=>{
+      console.log(err.message);
+    })  
+  }
+
   return (
     <div className="addNewUser">
-      <Form>
+      <Form onSubmit={handleAddUser}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Name" />
+          <Form.Control onChange={(e)=> setName(e.target.value)} value={name} type="text" placeholder="Enter Name" />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control  onChange={(e)=> setEmail(e.target.value)} value={email}  type="email" placeholder="Enter email" />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
         <label className="me-3" >Choose Gender</label>
-        <select  className="me-3" name="" id="">
+        <select value={gender}  onChange={(e)=> setGender(e.target.value)}   className="me-3" name="" id="">
             <option value="gender">Gender</option>
-            <option value="gender">Male</option>
-            <option value="gender">Female</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
         </select>
         <label className="me-3" >Status</label>
-        <select className="" name="" id="">
-            <option value="gender">Active</option>
-            <option value="gender">UnActive</option>
+        <select value={status} onChange={(e)=> setStatus(e.target.value)} className="" name="" id="">
+            <option value="active">status</option>
+            <option value="active">Active</option>
+            <option value="unActive">UnActive</option>
         </select>
     
         <Button style={{width : '100%'}} className="d-block mt-4" variant="primary" type="submit">
